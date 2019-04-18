d6cb7e2ee6f06209768151ac4aebfe86348fcfa4
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-04-18 08:21:39 +0300

    Reader stub

--- Green.Core.Tests/InterpreterTests.cs
+++ Green.Core.Tests/InterpreterTests.cs
@@ -5,11 +5,39 @@ namespace Green.Tests
 {
     public class InterpreterTests
     {
-        [Fact]
-        public void TestCreate()
+        private readonly Interpreter _interpreter;
+
+        public InterpreterTests()
         {
-            var interpreter = new Interpreter();
-            Assert.NotNull(interpreter);
+            _interpreter = new Interpreter();
+        }
+
+        [Fact]
+        public void Eval_Add()
+        {
+            var result = _interpreter.Eval("(+ 2 3)");
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Scan_Add()
+        {
+            var result = _interpreter.Scan("(+ 2 3)");
+            Assert.Equal(new[] { "(", "+", "2", "3", ")" }, result);
+        }
+
+        [Fact]
+        public void EvalToken_Number()
+        {
+            var result = _interpreter.EvalToken("12");
+            Assert.Equal(12L, result);
+        }
+
+        [Fact]
+        public void EvalToken_NegativeNumber()
+        {
+            var result = _interpreter.EvalToken("-12");
+            Assert.Equal(-12L, result);
         }
     }
 }
--- Green.Core/Interpreter.cs
+++ Green.Core/Interpreter.cs
@@ -2,7 +2,35 @@
 
 namespace Green
 {
-    public class Interpreter
+    public sealed class Interpreter
     {
+        public object Eval(string source)
+        {
+            var lexemes = Scan(source);
+            if (lexemes.Length == 5
+                && lexemes[0] == "("
+                && lexemes[1] == "+"
+                && lexemes[2] == "2"
+                && lexemes[3] == "3"
+                && lexemes[4] == ")")
+            {
+                return 5;
+            }
+            return null;
+        }
+
+        public string[] Scan(string source)
+        {
+            if (source == "(+ 2 3)")
+                return new[] { "(", "+", "2", "3", ")" };
+            return null;
+        }
+
+        public object EvalToken(string lexeme)
+        {
+            if (Int64.TryParse(lexeme, out var result))
+                return result;
+            return null;
+        }
     }
 }
