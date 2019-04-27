12c89b29c4896c8efd1883cdfb6e208322617ffc
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-04-27 15:33:50 +0300

    Implement LookAheadEnumerator

---
+++ Green.Core/LookAhead.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Green
+{
+	public static class LookAhead
+	{
+		public static LookAheadEnumerator<T> Enumerate<T>(IEnumerable<T> source) => new LookAheadEnumerator<T>(source);
+	}
+}
