5b5b6945100313cda1e02bf963d7b8aa382f0f61
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-04-27 16:31:48 +0300

    Implement SourcePositionBuilder

---
+++ Green.Core/SourcePosition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Green
+{
+    public struct SourcePosition
+    {
+        public readonly int Position;
+        public readonly int Line;
+        public readonly int Column;
+
+        public SourcePosition(int position,
+                              int line,
+                              int column)
+        {
+            Position = position;
+            Line = line;
+            Column = column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is SourcePosition other)
+                return Equals(other);
+            return false;
+        }
+
+        public bool Equals(SourcePosition other)
+        {
+            return Position == other.Position
+                && Line == other.Line
+                && Column == other.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Position, Line, Column).GetHashCode();
+        }
+    }
+}
---
+++ Green.Core/SourcePositionBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace Green
+{
+    public sealed class SourcePositionBuilder
+    {
+        private SourcePosition _current;
+
+        public SourcePosition Current => _current;
+
+        public void Update(char c)
+        {
+            if (c == '\n')
+            {
+                _current = new SourcePosition(
+                    position: _current.Position + 1,
+                    line: _current.Line + 1,
+                    column: 0);
+            }
+            else
+            {
+                _current = new SourcePosition(
+                    position: _current.Position + 1,
+                    line: _current.Line,
+                    column: _current.Column + 1);
+            }
+
+        }
+    }
+}
