aea1554c55558dd4ff8a938aff9f6d8f1d111b26
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-05-03 10:20:49 +0300

    REPL stub

---
+++ Green.Repl/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Green.Repl
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Green REPL");
+        }
+    }
+}
