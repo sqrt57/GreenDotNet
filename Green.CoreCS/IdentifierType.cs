22720df49703e2edbf1f24288249d71fe1e15b2d
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-06-04 23:58:34 +0300

    Remove no longer used classes

--- Green.CoreCS/IdentifierType.cs
+++
@@ -1,8 +0,0 @@
-namespace Green
-{
-    public enum IdentifierType
-    {
-        Identifier,
-        Keyword,
-    }
-}
--- Green.CoreCS/LookAhead.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Green
-{
-    public static class LookAhead
-    {
-        public static LookAheadEnumerator<T> Enumerate<T>(IEnumerable<T> source) => new LookAheadEnumerator<T>(source);
-    }
-}
