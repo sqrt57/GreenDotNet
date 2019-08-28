5a7544fcc0195618ee1c574752787ff3c2ab39e4
Dmitry Grigoryev <dmitriy57@gmail.com>
2019-08-28 07:35:44 +0300

    Compile If from IR to bytecode

--- Green.Core/Compile.fs
+++ Green.Core/Compile.fs
@@ -7,17 +7,19 @@ open Bytecode
 
 module Compile =
 
-    type CodeBlock<'constInfo,'varInfo> =
-        | Const of 'constInfo
-        | VarRef of 'varInfo
-        | Call of func : CodeBlock<'constInfo,'varInfo> * args : CodeBlock<'constInfo,'varInfo> list
+    type CodeBlock<'con,'var> =
+        | Const of 'con
+        | VarRef of 'var
+        | Call of func : CodeBlock<'con,'var> * args : CodeBlock<'con,'var> list
+        | If of cond : CodeBlock<'con,'var> * then_ : CodeBlock<'con,'var> * else_ : CodeBlock<'con,'var>
 
     module CodeBlock =
 
-        type Cata<'constInfo,'varInfo,'result> =
-            abstract member cConst : info:'constInfo -> 'result
-            abstract member cVarRef : info:'varInfo -> 'result
+        type Cata<'con,'var,'result> =
+            abstract member cConst : info:'con -> 'result
+            abstract member cVarRef : info:'var -> 'result
             abstract member cCall : func:'result -> args:'result list -> 'result
+            abstract member cIf : cond:'result -> then_:'result -> elseBlock:'result -> 'result
 
         let rec cata (c:Cata<'a,'b,'r>) (block:CodeBlock<'a,'b>) : 'r =
             let recurse = cata c
@@ -28,33 +30,14 @@ module Compile =
                 let func = recurse funcBlock
                 let args = List.map recurse argsBlocks
                 c.cCall func args
+            | If (cond=cond; then_=then_; else_=elseBlock) ->
+                c.cIf (recurse cond) (recurse then_) (recurse elseBlock)
 
-        type Fold<'constInfo,'varInfo,'acc> =
-            abstract member fConst : acc:'acc -> info:'constInfo -> 'acc
-            abstract member fVarRef : acc:'acc -> info:'varInfo -> 'acc
-            abstract member fPreCall : acc:'acc -> 'acc
-            abstract member fPostCallFunc : acc:'acc -> 'acc
-            abstract member fPostCallArgs : acc:'acc -> 'acc
-
-        let rec fold (f : Fold<'a,'b,'acc>) (acc : 'acc) (block : CodeBlock<'a,'b>) : 'acc =
-            let recurse = fold f
-            match block with
-            | Const x -> f.fConst acc x
-            | VarRef x -> f.fVarRef acc x
-            | Call (func=funcBlock; args=argBlocks) ->
-                acc
-                |> f.fPreCall
-                |> fun acc -> recurse acc funcBlock
-                |> f.fPostCallFunc
-                |> fun acc -> List.fold recurse acc argBlocks
-                |> f.fPostCallArgs
-
-        type CataFold<'constInfo,'varInfo,'acc,'result> =
-            abstract member fConst : acc:'acc -> info:'constInfo -> 'acc * 'result
-            abstract member fVarRef : acc:'acc -> info:'varInfo -> 'acc * 'result
-            abstract member fPreCall : acc:'acc -> 'acc
-            abstract member fPostCallFunc : acc:'acc -> func:'result -> 'acc
-            abstract member fPostCallArgs : acc:'acc -> func:'result -> args:'result list -> 'acc * 'result
+        type CataFold<'con,'var,'acc,'result> =
+            abstract member fConst : acc:'acc -> info:'con -> 'acc * 'result
+            abstract member fVarRef : acc:'acc -> info:'var -> 'acc * 'result
+            abstract member fCall : acc:'acc -> func:'result -> args:'result list -> 'acc * 'result
+            abstract member fIf : acc:'acc -> cond:'result -> then_:'result -> elseBlock:'result -> 'acc * 'result
 
         let rec cataFold (f : CataFold<'a,'b,'acc,'result>) (acc : 'acc) (block : CodeBlock<'a,'b>) : 'acc * 'result =
 
@@ -64,17 +47,20 @@ module Compile =
             | Const x -> f.fConst acc x
             | VarRef x -> f.fVarRef acc x
             | Call (func=funcBlock; args=argBlocks) ->
-
                 let argFolder (acc,argsAcc) arg =
                     let acc,arg = recurse acc arg
                     (acc,arg::argsAcc)
 
-                let acc = f.fPreCall acc
                 let acc,func = recurse acc funcBlock
-                let acc = f.fPostCallFunc acc func
                 let acc,argsAcc = List.fold argFolder (acc,[]) argBlocks
                 let args = List.rev argsAcc
-                f.fPostCallArgs acc func args
+                f.fCall acc func args
+
+            | If (cond=condBlock; then_=thenBlock; else_=elseBlock) ->
+                let acc,cond = recurse acc condBlock
+                let acc,then_ = recurse acc thenBlock
+                let acc,else_ = recurse acc elseBlock
+                f.fIf acc cond then_ else_
 
     let rec syntaxToBlocks ({syntax=syntax} : 't SyntaxWithInfo) : CodeBlock<Value,string> option =
         match syntax with
@@ -92,9 +78,8 @@ module Compile =
             new CodeBlock.CataFold<Value, 'a, int * Value list, CodeBlock<int,'a>> with
                 member __.fConst ((num, objAcc)) constant = (num+1, constant::objAcc), Const num
                 member __.fVarRef acc info = acc, VarRef info
-                member __.fPreCall acc = acc
-                member __.fPostCallFunc acc _ = acc
-                member __.fPostCallArgs acc func args = acc, Call (func=func,args=args) }
+                member __.fCall acc func args = acc, Call (func=func,args=args)
+                member __.fIf acc cond then_ else_ = acc, If (cond=cond,then_=then_,else_=else_) }
         let (_num,constsAcc),result = CodeBlock.cataFold folder (0,[]) block
         result, (constsAcc |> List.rev |> List.toSeq)
 
@@ -103,9 +88,8 @@ module Compile =
             new CodeBlock.CataFold<'a, string, int * string list, CodeBlock<'a,int>> with
                 member __.fConst acc info = acc, Const info
                 member __.fVarRef ((num,nameAcc)) name = (num+1, name::nameAcc), VarRef num
-                member __.fPreCall acc = acc
-                member __.fPostCallFunc acc _ = acc
-                member __.fPostCallArgs acc func args = acc, Call (func=func,args=args) }
+                member __.fCall acc func args = acc, Call (func=func,args=args)
+                member __.fIf acc cond then_ else_ = acc, If (cond=cond,then_=then_,else_=else_) }
         let (_num,constsAcc),result = CodeBlock.cataFold folder (0,[]) block
         result, (constsAcc |> List.rev |> List.toSeq)
 
@@ -121,7 +105,20 @@ module Compile =
                             yield! arg
                         yield Checked.byte OpCode.Call1
                         yield Checked.byte <| List.length args
-                    } }
+                    }
+                member __.cIf cond thenBlock elseBlock =
+                    let t = List.ofSeq thenBlock
+                    let e = List.ofSeq elseBlock
+                    seq {
+                        yield! cond
+                        yield Checked.byte OpCode.JumpIfNot1
+                        yield Checked.byte <| List.length t
+                        yield! t
+                        yield Checked.byte OpCode.Jump1
+                        yield Checked.byte <| List.length e
+                        yield! e
+                    }
+        }
         CodeBlock.cata c block
 
     let compile (expr: 'T SyntaxWithInfo) : Block option =
